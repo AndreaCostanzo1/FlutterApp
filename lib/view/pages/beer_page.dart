d6bf85e45927db2b1c9c60c1ef04737b9554a47c
AndreaCostanzo1 <andrea1.costanzo@mail.polimi.it>
2019-06-05 01:30:01 +0200

    Started to decouple text themes from pages: Moved to styles directory

--- lib/view/pages/beer_page.dart
+++ lib/view/pages/beer_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_beertastic/view/pages/fragments/fragments_w480max_small/beer_bottom_bar.dart';
-import 'package:flutter_beertastic/view/pages/fragments/fragments_w480max_small/beer_main_fragment.dart';
+import 'package:flutter_beertastic/view/pages/fragments/fragments_w320max_small/beer_bottom_bar.dart';
+import 'package:flutter_beertastic/view/pages/fragments/fragments_w320max_small/beer_main_fragment.dart';
 import 'package:flutter_beertastic/view/pages/fragments/fragments_wmax_medium/beer_bottom_bar.dart';
 import 'package:flutter_beertastic/view/pages/fragments/fragments_wmax_medium/beer_main_fragment.dart';
-import 'package:flutter_beertastic/view/pages/utils/size_computer.dart';
+import 'package:flutter_beertastic/view/pages/styles/w320max_small/beer_page_style_small.dart';
+import 'package:flutter_beertastic/view/utils/size_computer.dart';
 
 Map beer = {
   'name': 'Beck\'s',
@@ -54,31 +55,7 @@ class _SmallPage extends StatelessWidget {
       ),
       //theme of the page
       data: Theme.of(context).copyWith(
-        textTheme: TextTheme(
-            title: TextStyle(
-              fontSize: 32.0,
-              color: Theme.of(context).textTheme.title.color,
-              fontWeight: FontWeight.bold,
-            ),
-            subtitle: TextStyle(
-              color: Theme.of(context).textTheme.subtitle.color,
-              fontSize: 11,
-              fontWeight: FontWeight.normal,
-            ),
-            display1: TextStyle(
-              color: Theme.of(context).textTheme.display1.color,
-              fontSize: 22.0,
-              fontWeight: FontWeight.bold,
-            ),
-            display2: TextStyle(
-              color: Theme.of(context).textTheme.display2.color,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold,
-            ),
-            overline: Theme.of(context).textTheme.overline.copyWith(
-                  fontSize: 12.0,
-                  letterSpacing: 0,
-                )),
+        textTheme: BeerPageTextThemeSmall.textTheme,
       ),
     );
   }
---
+++ lib/view/pages/styles/w320max_small/beer_page_style_small.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class BeerPageTextThemeSmall {
+  static const TextTheme textTheme = const TextTheme(
+    title: TextStyle(
+      fontSize: 32.0,
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+    ),
+    subtitle: TextStyle(
+      color: Colors.black45,
+      fontSize: 11,
+      fontWeight: FontWeight.normal,
+    ),
+    display1: TextStyle(
+      color: Color(0xffffca28),
+      fontSize: 22.0,
+      fontWeight: FontWeight.bold,
+    ),
+    display2: TextStyle(
+      color: Colors.white,
+      fontSize: 28.0,
+      fontWeight: FontWeight.bold,
+    ),
+    overline: TextStyle(
+      color: Colors.white70,
+      fontSize: 12.0,
+      letterSpacing: 0,
+    ),
+  );
+}
