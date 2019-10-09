33da87d104bf05dabf76b0e3e9b3255b156d469b
AndreaCostanzo1 <andrea1.costanzo@mail.polimi.it>
2019-10-09 13:19:10 +0200

    Started to design the home page: *Added various fonts *Only some of them are inserted in the pubspec

--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -1,5 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -50,12 +49,60 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold(backgroundColor: Color(0xfffffbf0),
       body: Container(
         child: ListView(
           scrollDirection: Axis.vertical,
           children: <Widget>[
-
+            Stack(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(15),
+                  constraints: BoxConstraints.expand(height: 165),
+                  decoration: BoxDecoration(
+                      gradient: new LinearGradient(
+                          colors: [
+                            Theme.of(context).primaryColorLight,
+                            Theme.of(context).primaryColorDark
+                          ],
+                          begin: const FractionalOffset(1.0, 1.0),
+                          end: const FractionalOffset(0.2, 0.2),
+                          stops: [0.0, 1.0],
+                          tileMode: TileMode.clamp),
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(30),
+                          bottomRight: Radius.circular(30))),
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        _TitleBar(
+                            'Discover',
+                            TextStyle(
+                                fontFamily: 'Montserrat Bold',
+                                fontSize: 28,
+                                color: Color(0xf2f2f2f2)))
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 80),
+                  child: _BlogArticles(),
+                ),
+              ],
+            ),
+            SizedBox(height: 8),
+            _TitleBar(
+              'This week in Milan',
+              TextStyle(
+                  fontSize: 25,
+                  fontFamily: 'Montserrat Bold',
+                  color: Colors.black87),
+            ),
+            _Events(),
+            SizedBox(height: 24),
+            _BottomBar(),
           ],
         ),
       ), //center
@@ -63,4 +110,252 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
+class _BottomBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
 
+class _BlogArticles extends StatefulWidget {
+  @override
+  _BlogArticlesState createState() => _BlogArticlesState();
+}
+
+class _BlogArticlesState extends State<_BlogArticles> {
+  PageController pageController = PageController(viewportFraction: 0.86);
+
+  int currentPage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      margin: EdgeInsets.symmetric(vertical: 8),
+      child: PageView(
+        controller: pageController,
+        scrollDirection: Axis.horizontal,
+        children: data.map((article) {
+          bool activePage = data.indexOf(article) == currentPage;
+          return AnimatedContainer(
+            child: SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(top: 130, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.only(),
+                      child: Text(
+                        'My title',
+                        style: TextStyle(
+                            color: Color(0xF2F2F2F2),
+                            fontSize: 25,
+                            fontFamily: 'PlayfairDisplay Bold'),
+                      ),
+                    ),
+                    activePage
+                        ? Container(
+                            child: Text(
+                              'Lorem ipsum dolor sic amet',
+                              style: TextStyle(
+                                  color: Color(0xF2F2F2F2),
+                                  fontSize: 16,
+                                  fontFamily: 'Montserrat Regular'),
+                            ),
+                          )
+                        : Container(),
+                  ],
+                ),
+              ),
+            ),
+            margin: EdgeInsets.only(
+                top: activePage ? 0 : 20,
+                right: 30,
+                bottom: activePage ? 10 : 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              image: DecorationImage(
+                colorFilter: ColorFilter.mode(Colors.black26, BlendMode.darken),
+                image: NetworkImage(article['image']),
+                fit: BoxFit.cover,
+              ),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black87,
+                    blurRadius: activePage ? 5 : 0,
+                    offset: Offset(activePage ? 5 : 0, activePage ? 5 : 0)),
+              ],
+            ),
+            duration: Duration(milliseconds: 500),
+            curve: Curves.easeOutQuint,
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    currentPage = 0;
+    pageController.addListener(() => _changePage());
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+  _changePage() {
+    int currentPosition = pageController.page.round();
+    if (currentPage != currentPosition) {
+      setState(() {
+        currentPage = currentPosition;
+      });
+    }
+  }
+}
+
+class _TitleBar extends StatelessWidget {
+  final String title;
+  final TextStyle textStyle;
+
+  _TitleBar(this.title, this.textStyle);
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    return Container(
+      padding: EdgeInsets.only(top: 16, left: 16, bottom: 8),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    title,
+                    style: textStyle,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Events extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double cardWidth = MediaQuery.of(context).size.width * 0.87;
+    double cardHeight = 210;
+    double proportionImageTitle = 0.63;
+    double dateContainerSize = 63;
+    return Column(
+      children: data.map((brewery) {
+        return Container(
+          width: cardWidth,
+          margin: EdgeInsets.symmetric(vertical: 10),
+          height: cardHeight,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Stack(
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      height: cardHeight * proportionImageTitle,
+                      width: cardWidth,
+                      child: Image(
+                        image: NetworkImage(brewery['image']),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Container(
+                      height: cardHeight * (1 - proportionImageTitle),
+                      width: cardWidth,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                Container(
+                  width: dateContainerSize,
+                  height: dateContainerSize,
+                  margin: EdgeInsets.fromLTRB(
+                      cardWidth * 0.761, cardHeight * 0.47, 0, 0),
+                  child: Stack(
+                    children: <Widget>[
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                          color: Colors.white,
+                        ),
+                      ),
+                      Container(
+                        height: dateContainerSize*0.95,
+                        width: dateContainerSize,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              '11',
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontSize: 29,
+                                fontFamily: 'Open Sans SemiBold'
+                              ),
+                            ),
+                            Text(
+                              'JEN',
+                              style: TextStyle(
+                                  color: Colors.black87,
+                                  fontSize: 13,
+                                  fontFamily: 'Open Sans Bold'
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black45,
+                        blurRadius: 7,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Column( /*TODO*/),
+                ),
+              ],
+            ),
+          ),
+          //clipRRect
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xBB000000),
+                blurRadius: 10,
+                offset: Offset(5, 5),
+              ),
+            ],
+          ),
+        );
+      }).toList(),
+      crossAxisAlignment: CrossAxisAlignment.center,
+    );
+  }
+}
