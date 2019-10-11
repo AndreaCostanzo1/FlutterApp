6e6bbc199b3453061f55e1b23696128ea71f49eb
AndreaCostanzo1 <andrea1.costanzo@mail.polimi.it>
2019-10-11 02:42:45 +0200

    Improved add to favourite button in beer details
    NOTE: A nested Inkwell can improve the look of the other button, but
    Nested Inkwells don't work well toghether.
    See: https://github.com/flutter/flutter/issues/14417

--- lib/view/pages/beer_details_page.dart
+++ lib/view/pages/beer_details_page.dart
@@ -2,7 +2,6 @@ import 'package:flare_flutter/flare_actor.dart';
 
 import 'package:flutter/material.dart';
 
-import 'package:flutter_beertastic/view/components/buttons/custom_backgrounded_icon_button.dart';
 import 'package:flutter_beertastic/view/components/icons/custom_icons.dart';
 
 Color bottomBarColor = Colors.amber[300];
@@ -189,6 +188,70 @@ class DetailsScreen extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class CustomBackgroundedIconButton extends StatelessWidget {
+  final Radius topLeftRadius;
+  final Radius topRightRadius;
+  final Radius bottomLeftRadius;
+  final Radius bottomRightRadius;
+  final Color backgroundColor;
+  final Color outlineColor;
+  final double buttonHeight;
+
+  CustomBackgroundedIconButton(
+      {this.backgroundColor = const Color(0xff2c2731),
+        this.outlineColor = Colors.white,
+        this.topLeftRadius = Radius.zero,
+        this.topRightRadius = Radius.zero,
+        this.bottomLeftRadius = Radius.zero,
+        this.bottomRightRadius = Radius.zero,
+        this.buttonHeight = 80.0});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      height: buttonHeight,
+      duration: Duration(milliseconds: 300),
+      width: MediaQuery.of(context).size.width / 2,
+      curve: Curves.ease,
+      decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.only(
+              topLeft: topLeftRadius,
+              topRight: topRightRadius,
+              bottomLeft: bottomLeftRadius,
+              bottomRight: bottomRightRadius)),
+      child: Material(
+        color: Colors.transparent,
+
+        child: InkWell(
+          borderRadius: BorderRadius.only(
+              topLeft: topLeftRadius,
+              topRight: topRightRadius,
+              bottomLeft: bottomLeftRadius,
+              bottomRight: bottomRightRadius),
+          onTap: () => print('tap'),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.favorite,
+                color: outlineColor,
+              ),
+              SizedBox(
+                width: 6.0,
+              ),
+              Text(
+                'add to favorites',
+                style: TextStyle(color: outlineColor),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
