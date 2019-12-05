a63dc08b827270d64ed30e95bcd2606e4f5333a1
AndreaCostanzo1 <andrea1.costanzo@mail.polimi.it>
2019-12-05 13:00:27 +0100

    Minor changes in the profile page

--- lib/view/pages/fragments/fragments_wmax_medium/profile_fragment.dart
+++ lib/view/pages/fragments/fragments_wmax_medium/profile_fragment.dart
@@ -28,25 +28,34 @@ class _ProfileSection extends StatelessWidget {
                     Theme.of(context).primaryColorDark
                   ],
                   begin: const FractionalOffset(1.0, 1.0),
-                  end: const FractionalOffset(0.2, 0.2),
-                  stops: [0.0, 1.0],
+                  end: const FractionalOffset(0.1, 0.1),
+                  stops: [0.0, 0.8],
                   tileMode: TileMode.clamp),
               ),
           child: Container(
             padding: EdgeInsets.all(15),
             child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Container(
-                  height: 90,
-                  width: 90,
+                  height: 95,
+                  width: 95,
                   margin: EdgeInsets.only(top: 11),
                   child: ClipOval(
                     child: Image(image: NetworkImage('https://images.unsplash.com/photo-1548560781-a7a07d9d33db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=581&q=80'),fit: BoxFit.cover,),
                   ),
                 ),
-                Column(
-                  //TODO: ADD NAME AND MAIL
+                Container(
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("Andrea Costanzo"),
+                      Text("andrea.costanzo96@gmail.com")
+                    ],
+                  ),
                 )
               ],
             ),
