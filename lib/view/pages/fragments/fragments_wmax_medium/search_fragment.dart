173e7f2ebde9454e22d0929a618643301cd26fa2
AndreaCostanzo1 <andrea1.costanzo@mail.polimi.it>
2020-08-11 15:50:26 +0200

    Removed unused references in pages/fragments

--- lib/view/pages/fragments/fragments_wmax_medium/search_fragment.dart
+++ lib/view/pages/fragments/fragments_wmax_medium/search_fragment.dart
@@ -4,38 +4,6 @@ import 'package:flutter_beertastic/model/beer.dart';
 
 import '../../beer_page.dart';
 
-List data = [
-  {
-    'name': 'Antelope Canyon',
-    'image':
-        'https://images.unsplash.com/photo-1527498913931-c302284a62af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
-    'description':
-        'Over the years, Lover Antelope Canyon has become a favorite gathering pace for photographers tourists, and visitors from the world.',
-    'date': 'Mar 20, 2019',
-    'rating': '4.7',
-    'cost': '\$40.00'
-  },
-  {
-    'name': 'Genteng Lembang',
-    'image':
-        'https://images.unsplash.com/photo-1548560781-a7a07d9d33db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=581&q=80',
-    'description':
-        'Over the years, Lover Antelope Canyon has become a favorite gathering pace for photographers tourists, and visitors from the world.',
-    'date': 'Mar 24, 2019',
-    'rating': '4,83',
-    'cost': '\$50.00'
-  },
-  {
-    'name': 'Kamchatka Peninsula',
-    'image':
-        'https://images.unsplash.com/photo-1542869781-a272dedbc93e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=983&q=80',
-    'description':
-        'Over the years, Lover Antelope Canyon has become a favorite gathering pace for photographers tourists, and visitors from the world.',
-    'date': 'Apr 18, 2019',
-    'rating': '4,7',
-    'cost': '\$30.00'
-  },
-];
 
 class SearchFragment extends StatefulWidget {
   @override
--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -2,43 +2,9 @@ import 'dart:collection';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_beertastic/view/pages/article_page.dart';
 import 'package:flutter_beertastic/view/pages/fragments/fragments_wmax_medium/home_fragment.dart';
 import 'package:flutter_beertastic/view/pages/fragments/fragments_wmax_medium/profile_fragment.dart';
 import 'package:flutter_beertastic/view/pages/fragments/fragments_wmax_medium/search_fragment.dart';
-
-List data = [
-  {
-    'name': 'Antelope Canyon',
-    'image':
-        'https://images.unsplash.com/photo-1527498913931-c302284a62af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
-    'description':
-        'Over the years, Lover Antelope Canyon has become a favorite gathering pace for photographers tourists, and visitors from the world.',
-    'date': 'Mar 20, 2019',
-    'rating': '4.7',
-    'cost': '\$40.00'
-  },
-  {
-    'name': 'Genteng Lembang',
-    'image':
-        'https://images.unsplash.com/photo-1548560781-a7a07d9d33db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=581&q=80',
-    'description':
-        'Over the years, Lover Antelope Canyon has become a favorite gathering pace for photographers tourists, and visitors from the world.',
-    'date': 'Mar 24, 2019',
-    'rating': '4,83',
-    'cost': '\$50.00'
-  },
-  {
-    'name': 'Kamchatka Peninsula',
-    'image':
-        'https://images.unsplash.com/photo-1542869781-a272dedbc93e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=983&q=80',
-    'description':
-        'Over the years, Lover Antelope Canyon has become a favorite gathering pace for photographers tourists, and visitors from the world.',
-    'date': 'Apr 18, 2019',
-    'rating': '4,7',
-    'cost': '\$30.00'
-  },
-];
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
