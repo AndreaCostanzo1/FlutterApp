ac260c4ab38f53f4f97eed78c767cf96b4031ea0
AndreaCostanzo1 <andrea1.costanzo@mail.polimi.it>
2020-08-11 22:00:40 +0200

    Blocked device orientation: Portrait up Portrait down
    //Landscape not admitted

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
 import 'package:flutter_beertastic/view/pages/authentication_page.dart';
 import 'package:flutter_beertastic/view/pages/home_page.dart';
@@ -19,6 +20,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return StreamBuilder<FirebaseUser>(
         stream: FirebaseAuth.instance.onAuthStateChanged,
         builder: (context, snapshot) {
